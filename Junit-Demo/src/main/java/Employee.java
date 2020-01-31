453d5893be87c55f56271c10cb8457371ac26a87
ravisah364 <59965041+ravisah364@users.noreply.github.com>
2020-01-31 15:48:25 +0530

    Add files via upload

---
+++ Junit-Demo/src/main/java/Employee.java
@@ -0,0 +1,29 @@
+
+public class Employee {
+	private int Id;
+	private String Name;
+	
+	public Employee(int id, String name) {
+		super();
+		Id = id;
+		Name = name;
+	}
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	
+	
+
+}
---
+++ Junit-Demo/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Employee emp=new Employee();
+		emp.setId(101);
+		emp.setName("Ravi");
+		System.out.println(emp.getId() +emp. getName());
+	}
+
+}
